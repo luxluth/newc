e66dd8c091c1e4ea5463741bb94aece948d9ea67
luxluth <delphin.blehoussi93@gmail.com>
2023-01-29 13:56:15 +0100

    Template

---
+++ Makefile
@@ -0,0 +1,5 @@
+# Silent mode
+.SILENT:
+run:
+	gcc src/main.c -o bin/main
+	./bin/main
---
+++ src/main.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <string.h>
+
+int main(int argc, char *argv[])
+{
+    printf("Hello world\n");
+    return 0;
+}
